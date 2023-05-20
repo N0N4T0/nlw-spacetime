a988be37211fc298e90849d4e5edd59605de2b9c
Lucio <luciononato@hotmail.com>
2023-05-20 15:20:55 -0300

    fix: move anchor tag to inside tag p

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -14,10 +14,12 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
       />
 
-      <p className="max-w-[140px] text-sm leading-snug">{name}</p>
-      <a href="" className="block text-red-400 hover:text-red-300">
-        Quero sair
-      </a>
+      <p className="max-w-[140px] text-sm leading-snug">
+        {name}
+        <a href="" className="block text-red-400 hover:text-red-300">
+          Quero sair
+        </a>
+      </p>
     </div>
   )
 }
