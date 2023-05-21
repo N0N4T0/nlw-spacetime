2c32e03520ca73989d4ace816fedb6b31f6c812f
Lucio <luciononato@hotmail.com>
2023-05-21 09:06:11 -0300

    fix: add host

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,6 @@ app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
-  .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+  .then(() => console.log('🚀 HTTP server running'))
